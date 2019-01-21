bb46cf5c9529dab91551d2daa8c42159745ed4ea
James Ward <james.ward@hotmail.com>
2019-01-21 23:10:36 +0000

    create tmp file change

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -3,9 +3,13 @@ file { '/usr/local/bin/run-puppet':
   source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
   mode   => '0755',
 }
+file { '/tmp/hello.txt':
+  ensure  => 'file',
+  content => 'hello james',
+}
 
 cron { 'run-puppet':
   command => '/usr/local/bin/run-puppet',
   hour    => '*',
-  minute  => '*/15',
+  minute  => '*/1',
 }
